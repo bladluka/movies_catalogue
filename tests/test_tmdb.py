8631f5e61f209a5bacaa3bf45037c003fc628817
Łukasz <blada.lukasz@gmail.com>
2021-04-26 22:50:51 +0200

    Filmoteka

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -34,4 +34,11 @@ def test_get_single_movie_cast(monkeypatch):
    assert cast == mock_cast['cast'][:8]
 
 
+@pytest.mark.parametrize("list_type", ("top_rated", "upcoming", "popular", "now_playing"))
+def test_movies_list(monkeypatch, list_type):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
 
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
